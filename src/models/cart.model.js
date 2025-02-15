336844102aee97f7d2defcb9c0e2e17aef5e7748
Javier Veron <javier.veron@gmail.com>
2025-02-14 23:38:49 -0300

    Proyecto Final

--- src/models/cart.model.js
+++ src/models/cart.model.js
@@ -1,7 +1,17 @@
 import mongoose from "mongoose"
 
 const cartSchema = new mongoose.Schema({
-    products:Array
-})
+    products:{
+        type:[
+            {
+                product:{
+                    type:mongoose.Schema.Types.ObjectId,
+                    ref:"products"
+                }
+            }
+        ],
+        default:[]
+    }
+});
 
 export const cartModel = mongoose.model("cart", cartSchema);
--- src/routes/viewsRouter.js
+++ src/routes/viewsRouter.js
@@ -5,7 +5,8 @@ const viewsRouter = Router();
 const PM = new ProductManager();
 
 viewsRouter.get("/", async (req, res) => {
-    let products = await PM.getProducts();        
+    const {limit, page, query, sort} = req.query; 
+    let products = await PM.getProducts(limit, page, query, sort);
 
     res.render("home", {products:products});
 })
