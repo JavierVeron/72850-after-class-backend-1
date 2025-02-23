ba7a8bfb400107e4806a4b49cda46906c75e120e
Javier Veron <javier.veron@gmail.com>
2025-02-23 13:44:42 -0300

    Proyecto Final #2

--- src/app.js
+++ src/app.js
@@ -25,7 +25,7 @@ app.use("/api/products", productsRouter);
 app.use("/api/carts", cartsRouter);
 app.use("/", viewsRouter);
 
-mongoose.connect("mongodb+srv://javierveron:Xavi123!@cluster0.4sss6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+mongoose.connect();
 
 socketServer.on("connection", async socket => {
     const PM = new ProductManager();
